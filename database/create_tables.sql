db189c9cd91d53fa1b032e6cb7e00367669ef24f
wtc <bematjhb025@student.wethinkcode.co.za>
2026-07-06 14:48:09 +0200

    restructured folders

--- database/create_tables.sql
+++ database/create_tables.sql
@@ -1,33 +1,33 @@
 CREATE TABLE customers(
     customer_id INT PRIMARY KEY,
-    full_name VARCHAR(100),
-    province VARCHAR(100),
-    join_date date
+    full_name VARCHAR(100) NOT NULL,
+    province VARCHAR(100) NOT NULL,
+    join_date date NOT NULL
 );
 
 CREATE TABLE accounts(
     account_id INT PRIMARY KEY,
-    customer_id INT,
-    account_type VARCHAR(100),
-    open_date DATE,
+    customer_id INT NOT NULL,
+    account_type VARCHAR(100) NOT NULL,
+    open_date DATE NOT NULL,
 
     CONSTRAINT fk_accounts_customers 
         FOREIGN KEY customer_id 
-        REFERENCES customers.customer_id
+        REFERENCES customers(customer_id)
 );
 
 CREATE TABLE transactions(
     transaction_id INT PRIMARY KEY,
-    account_id INT ,
-    transaction_date DATE,
-    transaction_type VARCHAR(100),
+    account_id INT NOT NULL,
+    transaction_date DATE NOT NULL,
+    transaction_type VARCHAR(100) NOT NULL,
     merchant_name VARCHAR(100),
-    amount INT,
-    reference VARCHAR(100)
-    balance_after_transaction INT,
+    amount INT NOT NULL,
+    reference VARCHAR(100) NOT NULL,
+    balance_after_transaction INT NOT NULL,
     if_fraud BOOLEAN,
 
     CONSTRAINT fk_transactions_accounts 
         FOREIGN KEY account_id 
-        REFERENCES accounts.account_id
+        REFERENCES accounts(account_id)
 );
