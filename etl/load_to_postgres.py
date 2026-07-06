cf09345fabafdae3961e16fcb71f1c3f9220cd9f
benitmatumona <polviedivine@gmail.com>
2026-07-06 23:32:38 +0200

    fixed load to postgres.py

--- etl/load_to_postgres.py
+++ etl/load_to_postgres.py
@@ -11,61 +11,67 @@ username = "postgres"
 password = ""
 database = "south_africa_bank"
 
-conn = psycopg2.connect(
-    dbname=database,
-    user=username,
-    password=password,
-    host="localhost"
-)    
 
-with conn.cursor() as cur:
-    cur.execute("BEGIN;")
-    for row in customers_df.itertuples():
-        cur.execute(
-            """
-            INSERT INTO customers (customer_id, full_name, province, join_date})
-            VALUES (%s, %s, %s, %s)
-            """,
-            (
-                row.customer_id, 
-                row.full_name, 
-                row.province, 
-                row.join_date
-             )
-        )
-        
-    for row in accounts_df.itertuples():
-        cur.execute(
-            """
-            INSERT INTO accounts (account_id, customer_id, account_type, open_date)
-            VALUES (%s, %s, %s, %s)
-            """,
-            (
-                row.transaction_id, 
-                row.customer_id, 
-                row.account_type, 
-                row.open_date
-             )
-        )
-        
-    for row in transactions_df.itertuples():
-        cur.execute(
-            """
-            INSERT INTO transactions (transaction_id, account_id, transaction_date, transaction_type,
-            merchant_name, amount, refernce, balance_after_transaction, is_fraud)
-            VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)
-            """,
-            (
-                row.transaction_id, 
-                row.account_id, 
-                row.transaction_date, 
-                row.transaction_type, 
-                row.merchant_name,
-                row.amount,
-                row.refernce,
-                row.balance_after_transaction,
-                row.is_fraud
-             )
-        )
+try:
+    conn = psycopg2.connect(
+        dbname=database,
+        user=username,
+        password=password,
+        host="localhost"
+    )    
+
+    with conn.cursor() as cur:
+        for row in customers_df.itertuples():
+            cur.execute(
+                """
+                INSERT INTO customers (customer_id, full_name, province, join_date)
+                VALUES (%s, %s, %s, %s)
+                """,
+                (
+                    row.customer_id, 
+                    row.full_name, 
+                    row.province, 
+                    row.join_date
+                )
+            )
+            
+        for row in accounts_df.itertuples():
+            cur.execute(
+                """
+                INSERT INTO accounts (account_id, customer_id, account_type, open_date)
+                VALUES (%s, %s, %s, %s)
+                """,
+                (
+                    row.account_id, 
+                    row.customer_id, 
+                    row.account_type, 
+                    row.open_date
+                )
+            )
+            
+        for row in transactions_df.itertuples():
+            cur.execute(
+                """
+                INSERT INTO transactions (transaction_id, account_id, transaction_date, transaction_type,
+                merchant_name, amount, refernce, balance_after_transaction, is_fraud)
+                VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)
+                """,
+                (
+                    row.transaction_id, 
+                    row.account_id, 
+                    row.transaction_date, 
+                    row.transaction_type, 
+                    row.merchant_name,
+                    row.amount,
+                    row.reference,
+                    row.balance_after_transaction,
+                    row.is_fraud
+                )
+            )
 
         cur.execute("COMMIT;")
+except Exception:
+    conn.rollback()
+
+finally:
+    conn.close()
