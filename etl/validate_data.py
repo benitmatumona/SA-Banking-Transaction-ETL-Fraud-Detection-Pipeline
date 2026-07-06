819f998ff6c950339d451f633807b2f08fef7459
benitmatumona <polviedivine@gmail.com>
2026-07-07 00:17:24 +0200

    upadted validate.py

---
+++ etl/validate_data.py
@@ -0,0 +1,34 @@
+import pandas as pd
+
+
+customers_df = pd.read_csv("data/raw/customers.csv")
+accounts_df = pd.read_csv("data/raw/accounts.csv")
+transactions_df = pd.read_csv("data/raw/transactions.csv")
+
+
+def remove_duplicates():
+    pass
+
+
+def handle_na_values():
+    pass
+
+
+def primanry_keys_valid():
+    pass
+
+
+def not_null_columns():
+    pass
+
+
+def no_empty_rows():
+    pass
+
+
+def no_missid_required_values():
+    pass
+
+
+def account_type_valid():
+    pass
