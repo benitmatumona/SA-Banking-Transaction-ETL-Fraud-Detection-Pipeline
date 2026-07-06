9f113e019116cd2bcf3bc2c6f9ac556ddd0e2e46
wtc <bematjhb025@student.wethinkcode.co.za>
2026-07-06 16:56:18 +0200

    restructured folders

--- etl/load_to_postgres.py
+++ etl/load_to_postgres.py
@@ -7,4 +7,26 @@ customers_df = pd.read_csv("data/raw/customers.csv")
 accounts_df = pd.read_csv("data/raw/accounts.csv")
 transactions_df = pd.read_csv("data/raw/transactions.csv")
 
+username = "postgres"
+password = ""
+database = "south_africa_bank"
 
+conn = psycopg2.connect(
+    dbname=database,
+    user=username,
+    password=password,
+    host="localhost"
+)    
+
+with conn.cursor() as cur:
+    for row in customers_df.itertuples():
+        cur.execute(
+            "INSERT INTO accounts (customer_id, full_name, province, join_date)"
+            f"VALUES ({row.customer_id}, {row.full_name}, {row.province}, {row.join_date});"
+            )
+        
+    for row in accounts_df.itertuples():
+        cur.execute(
+            "INSERT INTO accounts (customer_id, full_name, province, join_date)"
+            f"VALUES ({row.customer_id}, {row.full_name}, {row.province}, {row.join_date});"
+            )
