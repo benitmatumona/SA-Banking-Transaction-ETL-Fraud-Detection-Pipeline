80f11cd726549807d70be326a1537cfd9e230d84
wtc <bematjhb025@student.wethinkcode.co.za>
2026-07-21 15:00:53 +0200

    cleaned code

--- src/etl/load.py
+++ src/etl/load.py
@@ -63,52 +63,62 @@ def connect(
 
 
 def load_customers(customers_df: pd.DataFrame, cur: Cursor) -> None:
-    values = [
+    rows = [
         (row.customer_id, row.full_name, row.province, row.join_date)
         for row in customers_df.itertuples()
     ]
     
     execute_values(
+        cur,
         """
         INSERT INTO customers (customer_id, full_name, province, join_date)
         VALUES %s
         """,
-        values,
+        rows,
     )
 
 
 def load_accounts(accounts_df: pd.DataFrame, cur: Cursor) -> None:    
-    for row in accounts_df.itertuples():
-        cur.execute(
-            """
-            INSERT INTO accounts (account_id, customer_id, account_type, open_date)
-            VALUES (%s, %s, %s, %s)
-            """,
-            (row.account_id, row.customer_id, row.account_type, row.open_date),
-        )
+    rows = [
+        (row.account_id, row.customer_id, row.account_type, row.open_date)
+        for row in accounts_df.itertuples()
+    ]
+    
+    execute_values(
+        cur,
+        """
+        INSERT INTO accounts (account_id, customer_id, account_type, open_date)
+        VALUES %s
+        """,
+        rows
+    )
 
 
 def load_transactions(transactions_df: pd.DataFrame, cur: Cursor) -> None:
-    for row in transactions_df.itertuples():
-        cur.execute(
-            """
-            INSERT INTO transactions (transaction_id, account_id, transaction_date, transaction_type,
-            transaction_channel, merchant_name, amount, reference, balance_after_transaction, is_fraud)
-            VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
-            """,
-            (
-                row.transaction_id,
-                row.account_id,
-                row.transaction_date,
-                row.transaction_type,
-                row.transaction_channel,
-                row.merchant_name,
-                row.amount,
-                row.reference,
-                row.balance_after_transaction,
-                row.is_fraud,
-            ),
+    rows = [
+        (
+            row.transaction_id,
+            row.account_id,
+            row.transaction_date,
+            row.transaction_type,
+            row.transaction_channel,
+            row.merchant_name,
+            row.amount,
+            row.reference,
+            row.balance_after_transaction,
+            row.is_fraud,
         )
+    for row in transactions_df.itertuples()
+    ]
+    execute_values(    
+        cur,
+        """
+        INSERT INTO transactions (transaction_id, account_id, transaction_date, transaction_type,
+        transaction_channel, merchant_name, amount, reference, balance_after_transaction, is_fraud)
+        VALUES %s
+        """,
+        rows
+    )
 
 
 if __name__ == "__main__":
