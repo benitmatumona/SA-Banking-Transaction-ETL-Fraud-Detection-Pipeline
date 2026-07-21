0abbd670651c5cbeabf8b3d2feb1a3519a465270
wtc <bematjhb025@student.wethinkcode.co.za>
2026-07-21 13:51:47 +0200

    cleaned code

--- src/etl/load.py
+++ src/etl/load.py
@@ -1,6 +1,7 @@
 import logging
 import pandas as pd
 import psycopg2
+from psycopg2.extensions import cursor
 
 
 def load(
@@ -10,21 +11,20 @@ def load(
 ) -> None:
     
     conn = None
-    load_customers(customers_df)
-    load_accounts(accounts_df)
-    load_transactions(transactions_df)
-
+    
     try:
         conn = connect(database, username, password, "localhost")
 
         with conn.cursor() as cur:
-            load(customers_df, accounts_df, transactions_df)
+            load_customers(customers_df, cur)
+            load_accounts(accounts_df, cur)
+            load_transactions(transactions_df, cur)
             conn.commit()
 
     except Exception as e:
         if conn:
             conn.rollback()
-        print(e)
+        logging.exception(f"Database load failed. {e}")
 
     finally:
         if conn:
@@ -36,14 +36,14 @@ def connect(
     username: str, 
     password:str, 
     host: str,
-) -> None:
+) -> psycopg2.extensions.connection:
     conn = psycopg2.connect(
         dbname=database, user=username, password=password, host=host
     )
     return conn
 
 
-def load_customers(df: pd.DataFrame) -> None:
+def load_customers(df: pd.DataFrame, cur: cursor) -> None:
     
     for row in df.itertuples():
         cur.execute(
@@ -55,7 +55,7 @@ def load_customers(df: pd.DataFrame) -> None:
         )
 
 
-def load_accounts(accounts_df: pd.DataFrame) -> None:    
+def load_accounts(accounts_df: pd.DataFrame, cur: cursor) -> None:    
     for row in accounts_df.itertuples():
         cur.execute(
             """
@@ -66,19 +66,20 @@ def load_accounts(accounts_df: pd.DataFrame) -> None:
         )
 
 
-def load_transactions() -> None:
+def load_transactions(transactions_df: pd.DataFrame, cur: cursor) -> None:
     for row in transactions_df.itertuples():
         cur.execute(
             """
             INSERT INTO transactions (transaction_id, account_id, transaction_date, transaction_type,
-            merchant_name, amount, reference, balance_after_transaction, is_fraud)
-            VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)
+            transaction_channel, merchant_name, amount, reference, balance_after_transaction, is_fraud)
+            VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
             """,
             (
                 row.transaction_id,
                 row.account_id,
                 row.transaction_date,
                 row.transaction_type,
+                row.transaction_channel,
                 row.merchant_name,
                 row.amount,
                 row.reference,
